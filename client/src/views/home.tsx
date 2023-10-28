b0e211ee89e322c4097fb8a7cceb6bdb31ca3782
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-10-28 15:05:31 +0300

    Creating products API Slice & Get Products Endpoint

--- client/src/views/home.tsx
+++ client/src/views/home.tsx
@@ -1,40 +1,39 @@
-import { useEffect, useState } from "react";
 import Card from "../components/card";
 import Rootview from "./root";
 import { Product } from "../interfaces/product_interfaces";
-import axios from "axios";
+import { useGetProductsQuery } from "../app/slices/productsApiSlice";
 
 const HomeView = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+  const { data: products, isLoading } = useGetProductsQuery({});
 
   return (
     <Rootview header="Pagrindinis">
-      <main className="flex-grow py-3 mx-5">
-        <div className="text-start">
-          <h1 className="text-2xl font-bold">Naujausios prekės</h1>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-5">
-          {products.map((product) => (
-            <Card
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              rating={product.rating}
-              price={product.price}
-              numReviews={product.numReviews}
-            />
-          ))}
-        </div>
-      </main>
+      {isLoading ? (
+        <>
+          <h2>Kraunama...</h2>
+        </>
+      ) : (
+        <>
+          <main className="flex-grow py-3 mx-5">
+            <div className="text-start">
+              <h1 className="text-2xl font-bold">Naujausios prekės</h1>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-5">
+              {products.map((product: Product) => (
+                <Card
+                  key={product._id}
+                  _id={product._id}
+                  image={product.image}
+                  name={product.name}
+                  rating={product.rating}
+                  price={product.price}
+                  numReviews={product.numReviews}
+                />
+              ))}
+            </div>
+          </main>
+        </>
+      )}
     </Rootview>
   );
 };
