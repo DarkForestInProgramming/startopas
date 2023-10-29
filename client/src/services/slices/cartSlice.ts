d1eaf293b7e56729686e45e1d0e05a72f210069c
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-10-29 11:24:39 +0200

    Creating Cart Utils file for best practices

--- client/src/services/slices/cartSlice.ts
+++ client/src/services/slices/cartSlice.ts
@@ -1,10 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { initialState } from "../constants/cartConstants";
 import { Product } from "../../interfaces/productInterfaces";
-
-const addDecimals = (num: number) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-};
+import { updateCart } from "../utils/cartUtils";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -24,30 +21,7 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-
-      //@desc Calculate items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce(
-          (acc: number, item: Product) => acc + item.price * item.qty,
-          0
-        )
-      );
-
-      //@desc Calculate shipping price (If order is over 50€ then free, else 5€ shipping)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 5);
-
-      //@desc Calculate tax price (21% tax)
-      state.taxPrice = addDecimals(
-        Number((0.21 * state.itemsPrice).toFixed(2))
-      );
-
-      //@desc Calculate total price
-      state.totalPrice =
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice).toFixed(2);
-
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
