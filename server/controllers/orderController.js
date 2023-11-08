c12c001dadd02c1aa7450b3a25fc9e5dab90fa3b
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-11-08 17:37:16 +0200

    Adding Create & Get Orders Logic

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -6,7 +6,37 @@ import Order from "../models/Order.js";
 // @access  Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("Užsakyme nėra prekių.");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc    Get logged in user orders
@@ -14,7 +44,8 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("get my orders");
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
@@ -22,7 +53,16 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("get order by id");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Užsakymas nerastas");
+  }
 });
 
 // @desc    Update order to paid
