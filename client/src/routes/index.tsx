3f4d23f9dd476eb480cd5fc39c8dc0dd7b5d4f72
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-11-08 12:30:43 +0200

    Creating Private Routes

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -11,17 +11,22 @@ import CartView from "../views/cart";
 import LoginView from "../views/login";
 import RegisterView from "../views/register";
 import ShippingView from "../views/shipping";
+import PrivateRoute from "./privateRoute";
 
 const AppRoutes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      {/* @desc Public Routes */}
       <Route index={true} path="/" element={<HomeView />} />
       <Route path="/product/:id" element={<ProductView />} />
       <Route path="/cart" element={<CartView />} />
       <Route path="/login" element={<LoginView />} />
       <Route path="/register" element={<RegisterView />} />
-      <Route path="/shipping" element={<ShippingView />} />
       <Route path="*" element={<NotFoundView />} />
+      {/* @desc Private Routes */}
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingView />} />
+      </Route>
     </Route>
   )
 );
