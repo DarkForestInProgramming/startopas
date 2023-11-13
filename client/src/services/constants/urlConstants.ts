7bb3f6fa88249db9db87ce4104c83a070d6d5d25
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-11-13 21:23:41 +0200

    Changing URL's To Production Domain

--- client/src/services/constants/urlConstants.ts
+++ client/src/services/constants/urlConstants.ts
@@ -1,5 +1,6 @@
-export const BASE_URL = "";
-export const PRODUCTS_URL = "/api/products";
-export const USERS_URL = "/api/users";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
+export const BASE_URL = "https://startopas-backend.vercel.app";
+export const PRODUCTS_URL = "https://startopas-backend.vercel.app/api/products";
+export const USERS_URL = "https://startopas-backend.vercel.app/api/users";
+export const ORDERS_URL = "https://startopas-backend.vercel.app/api/orders";
+export const PAYPAL_URL =
+  "https://startopas-backend.vercel.app/api/config/paypal";
